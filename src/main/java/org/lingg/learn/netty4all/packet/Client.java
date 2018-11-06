e153e56c2bf2454d33e18422285fc28896f6b460
starling113 <524881765@qq.com>
2018-11-06 14:01:05 +0800

    localhost

--- src/main/java/org/lingg/learn/netty4all/packet/Client.java
+++ src/main/java/org/lingg/learn/netty4all/packet/Client.java
@@ -5,25 +5,24 @@ import java.nio.ByteBuffer;
 
 public class Client {
 
-	public static void main(String[] args) throws Exception {
-		Socket socket = new Socket("10.1.6.8", 10101);
-		
-		String message = "hello我后光荣";
-		
-		byte[] bytes = message.getBytes();
-		
-		ByteBuffer buffer = ByteBuffer.allocate(5+4 + bytes.length);
-		buffer.put("anxin".getBytes());
-		buffer.putInt(bytes.length);
-		buffer.put(bytes);
-		
-		byte[] array = buffer.array();
-			
-		for(int i=0; i<1000; i++){
-			socket.getOutputStream().write(array);
-		}
-			
-		socket.close();
-	}
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 10101);
 
+        String message = "hello测试内容";
+
+        byte[] bytes = message.getBytes();
+
+        ByteBuffer buffer = ByteBuffer.allocate(5 + 4 + bytes.length);
+        buffer.put("anxin".getBytes());
+        buffer.putInt(bytes.length);
+        buffer.put(bytes);
+
+        byte[] array = buffer.array();
+
+        for (int i = 0; i < 1000; i++) {
+            socket.getOutputStream().write(array);
+        }
+
+        socket.close();
+    }
 }
