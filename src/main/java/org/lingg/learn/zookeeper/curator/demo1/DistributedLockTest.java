12ac22422832b58e31dcad706359d27c73eff094
starling113 <524881765@qq.com>
2018-11-14 16:55:31 +0800

    111

---
+++ src/main/java/org/lingg/learn/zookeeper/curator/demo1/DistributedLockTest.java
@@ -0,0 +1,39 @@
+package org.lingg.learn.zookeeper.curator.demo1;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+public class DistributedLockTest {
+    static int n = 500;
+
+    public static void secskill() {
+        System.out.println(--n);
+    }
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                DistributedLock lock = null;
+                try {
+                    lock = new DistributedLock("192.168.163.129:2181", "test1");
+                    lock.lock();
+                    secskill();
+                    System.out.println(Thread.currentThread().getName() + "正在运行");
+                } finally {
+                    if (lock != null) {
+                        lock.unlock();
+                    }
+                }
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(runnable);
+            t.start();
+        }
+    }
+}
